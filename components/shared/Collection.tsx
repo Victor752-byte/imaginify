ee1d2ea9f032317ad41692a295e9b916c1d357da
Victor752-byte <obiohavictor20@gmail.com>
2024-09-27 13:36:03 +0100

    fix collection component error

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -55,12 +55,12 @@ export const Collection = ({
         {hasSearch && <Search />}
       </div>
 
-      {/* {images.length > 0 ? (
+      {images.length > 0 ? (
         <ul className="collection-list">
           {images.map((image) => (
             <Card image={image} key={image.author._id} />
           ))}
-        </ul> */}
+        </ul> 
       ) : (
         <div className="collection-empty">
           <p className="p-20-semibold">Empty List</p>

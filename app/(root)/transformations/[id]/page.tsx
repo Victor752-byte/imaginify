3c2f5b23c4199600868a7ddd8f4f7103e28b8fbf
Victor752-byte <obiohavictor20@gmail.com>
2024-09-27 13:32:29 +0100

    fix import module error

--- app/(root)/transformations/[id]/page.tsx
+++ app/(root)/transformations/[id]/page.tsx
@@ -7,8 +7,9 @@ import TransformedImage from "@/components/shared/TransformedImage";
 import { Button } from "@/components/ui/button";
 import { getImageById } from "@/lib/actions/image.actions";
 import { getImageSize } from "@/lib/utils";
-import { DeleteConfirmation } from "@/components/shared/DeleteConfirmation";
+
 import { auth } from "@clerk/nextjs/server";
+import { DeleteConfirmation } from "@/components/shared/DeleteConfirmation";
 
 const ImageDetails = async ({ params: { id } }: SearchParamProps) => {
   const { userId } = auth();
